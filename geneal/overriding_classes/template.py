54a28d7ddc918284f35a6c434593a3478554a36a
diogomatoschaves <diogo_chaves@hotmail.com>
2020-06-01 19:48:27 +0200

    Add Travelling Salesman Problem solver

---
+++ geneal/overriding_classes/template.py
@@ -0,0 +1,14 @@
+class OverridingClassTemplate:
+    def fitness_function(self):
+        pass
+
+    def initialize_population(self):
+        pass
+
+    def create_offspring(
+        self, first_parent, sec_parent, crossover_pt, offspring_number
+    ):
+        pass
+
+    def mutate_population(self, population):
+        pass
